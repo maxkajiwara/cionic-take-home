39959b11f692cf168f324912faa34f6325eec12f
Max Kajiwara <maxkajiwara@gmail.com>
2022-10-05 18:20:51 -0700

    Set global styles

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -11,8 +11,9 @@ import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro'
 
 const CustomStyles = createGlobalStyle`
   body {
-    /* -webkit-tap-highlight-color: ${theme`colors.purple.500`}
-    ${tw`antialiased`} */
+    ${tw`antialiased`};
+		background-color: ${theme`colors[cionic-sky]`}
+		
   }
 `
 
