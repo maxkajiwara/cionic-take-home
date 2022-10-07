d41f9f2c16a6b7edd46f080b072ccee17f2502f9
Max Kajiwara <maxkajiwara@gmail.com>
2022-10-06 20:37:43 -0700

    Update theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 // tailwind.config.js
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-	],
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			colors: {
@@ -11,7 +8,11 @@ module.exports = {
 				'cionic-cyan': '#44B3E3',
 				'cionic-navy': '#374B70',
 				'cionic-graphite': '#1E201F',
-				'cionic-gray': '#656565',
+				'cionic-gray': {
+					400: '#D9D9D9',
+					500: '#C2C2C2',
+					600: '#656565',
+				},
 				'cionic-lime': '#BEDF2F',
 			},
 		},
